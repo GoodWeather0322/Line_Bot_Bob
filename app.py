6d5b5b4ed46975ef7b72d8da9ca5e2a284318bfa
GoodWeather <a82521112@gmail.com>
2018-01-27 20:39:39 +0800

    make it better

--- app.py
+++ app.py
@@ -44,38 +44,26 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    
+
+    # parse webhook body
     try:
-        handler.handle(body, signature)
+        events = parser.parse(body, signature)
     except InvalidSignatureError:
         abort(400)
 
-    # parse webhook body
-    #try:
-        #events = parser.parse(body, signature)
-    #except InvalidSignatureError:
-        #abort(400)
-
     # if event is MessageEvent and message is TextMessage, then echo text
-    #for event in events:
-        #if not isinstance(event, MessageEvent):
-            #continue
-        #if not isinstance(event.message, TextMessage):
-            #continue
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
 
-        #line_bot_api.reply_message(
-            #event.reply_token,
-            #TextSendMessage(text=event.message.text)
-        #)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
 
     return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
 
 
 if __name__ == "__main__":
