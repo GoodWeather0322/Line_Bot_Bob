0f3d5742340e91e4ee68182d077da27c012a9d76
GoodWeather <a82521112@gmail.com>
2018-01-28 13:46:12 +0800

    make it better

--- app_with_handler.py
+++ app_with_handler.py
@@ -50,10 +50,6 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=signature)
-    )
 
     # handle webhook body
     try:
